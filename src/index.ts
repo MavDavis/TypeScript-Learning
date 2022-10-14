412b62fe1f5d3b3ae20ca1001f877afed1133c52
MavDavis <ekwesimarvelous@gmail.com>
2022-10-14 15:05:35 +0100

    commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+// initialize a tsc config file
+// tsc --init
+
+// to watch a tsc file
+// tsc --watch (the name of the file)
+
+// my first tsc code
+// type of number
+let id:number = 1;
+console.log(id);
